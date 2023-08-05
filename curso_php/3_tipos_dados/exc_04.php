11a0ad09f106e45830e44f594528dad9eeeebaff
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-04 23:11:41 -0300

    inital commit

---
+++ curso_php/3_tipos_dados/exc_04.php
@@ -0,0 +1,14 @@
+<?php
+
+    $num_1 = 8.25;
+    $num_2 = 5.43;
+    $num_3 = 7.77;
+    $num_4 = 9;
+
+    if(is_float($num_1)) {
+        echo "$num_1 é float <br>";
+    }
+
+    if(is_float($num_4)) {
+        echo "$num_4 é float <br>";
+    }
---
+++ curso_php/3_tipos_dados/exc_05.php
@@ -0,0 +1,8 @@
+<?php
+
+    $nome = "João";
+    $sobrenome = 'da Silva';
+
+    echo 'O nome dele é $nome $sobrenome';
+    echo "<br>";
+    echo "O nome dele é $nome $sobrenome";
