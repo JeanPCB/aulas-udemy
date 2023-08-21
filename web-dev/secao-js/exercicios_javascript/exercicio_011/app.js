0906a61bf17b4f5a929f2e7c24e1327718d6764d
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-21 05:06:06 -0300

    finalizacao ate exc 13

--- web-dev/secao-js/exercicios_javascript/exercicio_011/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_011/app.js
@@ -7,4 +7,30 @@ Enunciado:
     No clique seguinte, que só pode acontecer numa img que não tenha símbolo,
     vamos apresentar o símbolo 'O'.
 ---------------------------------------------------------------------------- */
+altImgSource("click", getSeqElements("#img", 16), "./x.png", "./o.png")
 
+function getSeqElements(typeAndName, number) {
+    sameElArr = []
+
+    for(i = 1; i <= number; i++) {
+        sameElArr[i] = document.querySelector(typeAndName + i)
+    }
+
+    return sameElArr
+}
+
+function altImgSource(event, elArray, imgURL1, imgURL2) {
+    elArray.forEach(element => {
+        element.clickCount = 0
+
+        element.addEventListener(event, () => {
+            if(element.clickCount === 0) {
+                element.setAttribute("src", imgURL1)
+                element.clickCount = 1
+            } else {
+                element.setAttribute("src", imgURL2)
+                element.clickCount = 0
+            }
+        })
+    });
+}
--- web-dev/secao-js/exercicios_javascript/exercicio_013/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_013/app.js
@@ -7,3 +7,44 @@ Enunciado:
     sequencial e depois de inserido, o input deve ser limpo e receber o focus para
     nova inserção.
 ---------------------------------------------------------------------------- */
+
+const textPost = document.querySelector("#text_post")
+const btn = document.querySelector("button")
+
+setParagraph(textPost, btn, "#posts")
+
+function setParagraph(input, btn, father) {
+    let pCounter = 0
+
+    btn.addEventListener("click", () => {
+        if(!isEmptyField(input)) {
+            pCounter += 1
+            const newP = addEl("p")
+            newP.innerText = `${pCounter} - ${input.value}`
+            
+            appendEl(father, newP)
+            clearField(input)
+            focusOnField(input)
+        }
+    })
+}
+
+function addEl(element) {
+    return document.createElement(element)
+}
+
+function appendEl(father, child) {
+    return document.querySelector(father).appendChild(child)
+}
+
+function isEmptyField(field) {
+    return field.value === "" ? true : false
+}
+
+function focusOnField(field) {
+    field.focus()
+}
+
+function clearField(field) {
+    field.value = ""
+}
