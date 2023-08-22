2fc18673b4adc5d4b7aa67d6c986484b969a0b95
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-22 13:33:12 -0300

    finalizacao exc 14

--- web-dev/secao-js/exercicios_javascript/exercicio_014/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_014/app.js
@@ -8,4 +8,59 @@ Enunciado:
     nova inserção.
     Não são permitidas as palavras "teste", "obrigado" e "hoje"
 ---------------------------------------------------------------------------- */
+const addBtn = document.querySelector("button")
+const textPost = document.querySelector("#text_post")
+const posts = document.querySelector("#posts")
 
+displayParagraph(addBtn, textPost, posts)
+
+function displayParagraph(btn, input, father) {
+    let count = 1
+
+    btn.addEventListener("click", () => {
+        if(isInputFull(input)) {
+            if(!isValidInput(input)) {
+                alert("O valor do campo é inválido!")
+            } else {
+                const tempP = addEl("p")
+                tempP.textContent = count + " - " + input.value
+    
+                appendEl(father, tempP)
+                
+                count++
+            }
+        }
+
+        clearInput(input)
+        input.focus()
+    })
+}
+
+function addEl(element) {
+    return document.createElement(element)
+}
+
+function appendEl(father, child) {
+    return father.appendChild(child)
+}
+
+function clearInput(input) {
+    return input.value = ""
+}
+
+function isValidInput(input) {
+    switch (input.value) {
+        case "teste":
+        case "hoje":
+        case "obrigado":
+            return false
+
+        default:
+            return true
+    }
+}
+
+
+function isInputFull(input) {
+    return input.value != "" ? true : false
+}
