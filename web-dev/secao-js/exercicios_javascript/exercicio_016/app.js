b95eccf0dbed6f2f2becfe807cb4b7be2cd6e5c1
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-24 20:39:23 -0300

    finalizacao exc 16

--- web-dev/secao-js/exercicios_javascript/exercicio_016/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_016/app.js
@@ -10,5 +10,40 @@ Enunciado:
 
     NOTA: O HTML não pode ser alterado.
 ---------------------------------------------------------------------------- */
+const addBtn = document.querySelector("#btn-add")
+const clearBtn = document.querySelector("#btn-clear")
+const textField = document.querySelector("#text_post")
+const postsField = document.querySelector("#posts")
 
+setParagraph(textField, postsField)
+clearField(textField)
 
+function setParagraph(field, father) {
+    addBtn.addEventListener("click", () => {
+        if(!isFieldEmpty(field)) {
+            const tempP = addEl("p")
+            tempP.textContent = field.value
+    
+            appendEl(father, tempP)
+        }
+    })
+}
+
+function clearField(field) {
+    clearBtn.addEventListener("click", () => {
+        field.value = ""
+        field.focus()
+    })
+}
+
+function isFieldEmpty(field) {
+    return field.value === "" ? true : false
+}
+
+function addEl(element) {
+    return document.createElement(element)
+}
+
+function appendEl(father, child) {
+    return father.appendChild(child)
+}
