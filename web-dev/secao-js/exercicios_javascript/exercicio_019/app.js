fe6f64b7a63c7b264fc88a4e6874294ef04062a5
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-25 01:40:27 -0300

    finalizacao exc 19

--- web-dev/secao-js/exercicios_javascript/exercicio_019/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_019/app.js
@@ -10,3 +10,26 @@ Enunciado:
     NOTA: Não podes usar um ciclo neste exercício.
 ---------------------------------------------------------------------------- */
 let frutos = ['laranja', 'maçã', 'pêra', 'morango', 'ananás', 'limão', 'banana'];
+const searchBar = document.querySelector("#text_palavra")
+const result = document.querySelector("#resultado")
+const searchBtn = document.querySelector("button")
+
+function setResult(array, input, resultField) {
+    searchBtn.addEventListener("click", () => {
+        if(hasSearchItem(array, input.value)) {
+            resultField.textContent = "SIM"
+        } else {
+            resultField.textContent = "NÃO"
+        }
+    })
+}
+
+function hasSearchItem(array, item) {
+    if(array.find(el => el === item)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+setResult(frutos, searchBar, result)
