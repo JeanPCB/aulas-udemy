49103b7288a58223b0987e25cdf3bbdae073de63
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-25 04:56:27 -0300

    finalizacao exc 20

--- web-dev/secao-js/exercicios_javascript/exercicio_020/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_020/app.js
@@ -7,3 +7,24 @@ Enunciado:
     deve ficar amarela, indicando que o input está ativo. 
     Os inativos devem ficar a branco.
 ---------------------------------------------------------------------------- */
+const textInputs = document.querySelectorAll(".form-control")
+
+function changeFieldBg(event, target, color) {
+    target.addEventListener(event, (e) => {
+        if(isFieldEmpty(target)) {
+            e.target.style.backgroundColor = color
+        } else {
+            e.target.style.backgroundColor = "rgb(255, 255, 255)"
+        }
+    })
+}
+
+function isFieldEmpty(field) {
+    return field.value === "" ? true : false
+}
+
+textInputs.forEach( (element) => {
+    changeFieldBg("focus", element, "rgb(255, 255, 0)")
+    changeFieldBg("blur", element, "rgb(255, 255, 255)")
+    changeFieldBg("input", element, "rgb(255, 255, 0)")
+});
