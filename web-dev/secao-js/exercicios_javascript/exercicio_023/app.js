46b81cf3f6c7ad212808957eb304ddf47793db71
JeanPCB <jeanp.cebinel01@gmail.com>
2023-08-30 13:26:36 -0300

    finalizacao do exc 23

--- web-dev/secao-js/exercicios_javascript/exercicio_023/app.js
+++ web-dev/secao-js/exercicios_javascript/exercicio_023/app.js
@@ -8,3 +8,19 @@ Enunciado:
     Ao clicar em todas, todas as checkboxes devem ser checkadas.
     Ao clicar em nenhuma, todas as checkboxes devem ser descheckadas.
 ---------------------------------------------------------------------------- */
+const checkAllBtn = document.querySelector("#all")
+const uncheckAllBtn = document.querySelector("#none")
+const checkboxes = document.querySelectorAll("input[type=checkbox]")
+
+function changeCheckVal(btn, inputs, state) {
+    btn.addEventListener("click", () => {
+        inputs.forEach(element => {
+            if(element.checked != state) {
+                element.checked = state
+            }
+        });
+    } )
+}
+
+changeCheckVal(checkAllBtn, checkboxes, true)
+changeCheckVal(uncheckAllBtn, checkboxes, false)
