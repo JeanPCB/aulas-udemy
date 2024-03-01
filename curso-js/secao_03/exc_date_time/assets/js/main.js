b1e931f0832e188aefda9d9f5cb51e82a5f5a865
JeanPCB <jeanp.cebinel01@gmail.com>
2024-03-01 13:25:38 -0300

    correcao: exc switch date / func nativa

--- curso-js/secao_03/exc_date_time/assets/js/main.js
+++ curso-js/secao_03/exc_date_time/assets/js/main.js
@@ -1,28 +1,61 @@
+// Shortest solution (native function)
 function main() {
     // Variables
     const h1 = document.querySelector("h1");
 
-    const weekDays = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
-    const months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
-
     // Invoking
     showNowDate();
 
+    // Functions
     function createStringDate() {
         const date = new Date();
-        const fullDateString = `Data: ${dateParamToString(date.getDay(), weekDays)}, ${date.getDate()} de ${dateParamToString(date.getMonth(), months)} de ${date.getFullYear()}\nHora: ${date.getHours()}:${date.getMinutes()}`;
-
-        return fullDateString;
+        const options = {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        };
+        
+        return date.toLocaleString("pt-BR", options);
     }
 
-    // Functions
     function showNowDate() {
         h1.innerText = "";
         h1.innerText = createStringDate();
-    }
+    } 
+} main();
 
-    function dateParamToString(dateParam, paramStrings) {
-        return paramStrings[dateParam];
-    }
-}
-main()
+
+
+// // Custom solution
+// function main() {
+//     // Variables
+//     const h1 = document.querySelector("h1");
+
+//     const weekdays = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
+//     const months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+
+//     // Invoking
+//     showNowDate();
+
+//     function createStringDate() {
+//         const date = new Date();
+//         const fullDateString = `Data: ${dateParamToString(date.getDay(), weekdays)}, ${date.getDate()} de ${dateParamToString(date.getMonth(), months)} de ${date.getFullYear()}\nHora: ${date.getHours()}:${date.getMinutes()}`;
+
+//         return fullDateString;
+//     }
+
+//     // Functions
+//     function showNowDate() {
+//         h1.innerText = "";
+//         h1.innerText = createStringDate();
+//     }
+
+//     function dateParamToString(dateParam, paramStrings) {
+//         return paramStrings[dateParam];
+//     }
+// }
+// main();
