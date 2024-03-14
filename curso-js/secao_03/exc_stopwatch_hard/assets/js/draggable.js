c8d78b70fb65142a357b7b4eaabc08a2809c9b68
JeanPCB <jeanp.cebinel01@gmail.com>
2024-03-14 15:44:25 -0300

    melhoria: drag and drop - stopwatch hard

--- curso-js/secao_03/exc_stopwatch_hard/assets/js/draggable.js
+++ curso-js/secao_03/exc_stopwatch_hard/assets/js/draggable.js
@@ -3,19 +3,34 @@
     const titleDiv = document.querySelector(".title-div");
 
     // Invoking
-    titleDiv.addEventListener("mousedown", () => {
-        titleDiv.addEventListener("mousemove", dragMainContainer);
-    });
-
-    titleDiv.addEventListener("mouseup", () => {
-        titleDiv.removeEventListener("mousemove", dragMainContainer);
-    });
+    dragElement(titleDiv, mainContainer);
 
     // Functions
-    function dragMainContainer({movementX, movementY}) {
-        const getContainerStyle = window.getComputedStyle(mainContainer);
-        const leftValue = parseInt(getContainerStyle.left);
-        const topValue = parseInt(getContainerStyle.top);
-        mainContainer.style.left = `${leftValue + movementX}px`;
-        mainContainer.style.top = `${topValue + movementY}px`;
+    function dragElement(header = null, el) {
+        let startX = 0;
+        let startY = 0;
+        let finalX = 0;
+        let finalY = 0;
+
+        header.addEventListener("mousedown", (e) => {
+            startX = e.clientX;
+            startY = e.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        });
+
+        function elementDrag(e) {
+            finalX = startX - e.clientX;
+            finalY = startY - e.clientY;
+            startX = e.clientX;
+            startY = e.clientY;
+
+            el.style.top = (el.offsetTop - finalY) + "px";
+            el.style.left = (el.offsetLeft - finalX) + "px";
+        }
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
