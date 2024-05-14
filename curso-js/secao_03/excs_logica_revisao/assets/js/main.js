6bdd3f630c865cc0db4fc3c44f0b9389a22b7cf4
JeanPCB <jeanp.cebinel01@gmail.com>
2024-05-14 20:42:04 -0300

    finalizacao: exc01 - logica revisao

--- curso-js/secao_03/excs_logica_revisao/assets/js/main.js
+++ curso-js/secao_03/excs_logica_revisao/assets/js/main.js
@@ -1,7 +1,19 @@
 function main() {
+    // Variables
     const subBtnOne = document.querySelector("#btn-1");
+    const varA = document.querySelector("#var-a");
+    const varB = document.querySelector("#var-b");
+    let result = document.querySelector("#result");
 
+    // Invoking
     subBtnOne.addEventListener("click", e => {
         e.preventDefault();
+        result.innerText = getHigher(Number(varA.value), Number(varB.value));
     })
+
+    // Functions
+    function getHigher(a, b) {
+        return (a > b) ? a : b;
+    }
+
 } main();
